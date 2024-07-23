ac6c08d950de81992b2e0bf5835009c7f93c50ad
liangzhiquan <1298848072@qq.com>
2024-07-23 11:58:18 +0800

    first commit

---
+++ src/main/java/org/lzq/nyybackenduser/NyyBackendUserApplication.java
@@ -0,0 +1,13 @@
+package org.lzq.nyybackenduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyyBackendUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyyBackendUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lzq/nyybackenduser/NyyBackendUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lzq.nyybackenduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyyBackendUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
